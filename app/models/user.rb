4c728ce702dd24ea59b0ccbf207835f6896e61a3
EC2 Default User <ec2-user@ip-172-31-58-115.ec2.internal>
2020-10-19 10:54:07 +0000

    [finish]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,31 @@ class User < ApplicationRecord
   has_many :book_comments
   attachment :profile_image, destroy: false
 
+
+# foreign_key（FK）には、@user.xxxとした際に「@user.idがfollower_idなのかfollowed_idなのか」を指定します。
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  # @user.booksのように、@user.yyyで、
+  # そのユーザがフォローしている人orフォローされている人の一覧を出したい
+  has_many :following_users, through: :follower, source: :followed
+  has_many :follower_users, through: :followed, source: :follower
+
+  # ユーザーをフォローする
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  # フォロー確認をおこなう
+  def following?(user)
+    following_users.include?(user)
+  end
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,12 @@ Rails.application.routes.draw do
     resources :book_comments, only: [:create, :destroy]
   end
 
+  post 'follow/:id' => 'relationships#create', as: 'follow'
+  post 'unfollow/:id' => 'relationships#destroy', as: 'unfollow'
+
+  get 'follows/:id' => 'users#following', as: 'follows'
+  get 'followers/:id' => 'users#follower', as: 'followers'
+
   root 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
 # endを追加
